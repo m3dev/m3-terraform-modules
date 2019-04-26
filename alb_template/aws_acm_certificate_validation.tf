a7ea150390a463a7a7c1b1e1c1d9fa5c5fff4f2e
Seiya Yazaki <saiya.v6@gmail.com>
2019-04-26 10:35:58 +0900

    Fix ACM certificate validation

--- alb_template/aws_acm_certificate_validation.tf
+++ alb_template/aws_acm_certificate_validation.tf
@@ -1,9 +1,5 @@
 resource "aws_acm_certificate_validation" "cert" {
-  count = "${var.acm_certificate_validation ? 1 : 0}"
-
   certificate_arn = aws_acm_certificate.https-certificate.arn
-  validation_record_fqdns = [
-    aws_route53_record.https-certificate-validation.fqdn,
-  ]
+  validation_record_fqdns = aws_route53_record.https-certificate-validations.*.fqdn
 }
 
--- alb_template/aws_route53_record.tf
+++ alb_template/aws_route53_record.tf
@@ -1,10 +1,11 @@
-resource "aws_route53_record" "https-certificate-validation" {
-  name    = aws_acm_certificate.https-certificate.domain_validation_options[0].resource_record_name
-  type    = aws_acm_certificate.https-certificate.domain_validation_options[0].resource_record_type
+resource "aws_route53_record" "https-certificate-validations" {
+  count = 1 + length(var.acm_certificate_alternative_names)
+
+  name    = aws_acm_certificate.https-certificate.domain_validation_options[count.index].resource_record_name
+  type    = aws_acm_certificate.https-certificate.domain_validation_options[count.index].resource_record_type
   zone_id = var.route53_zone_id
   records = [
-    aws_acm_certificate.https-certificate.domain_validation_options[0].resource_record_value,
+    aws_acm_certificate.https-certificate.domain_validation_options[count.index].resource_record_value,
   ]
   ttl = 60
 }
-
