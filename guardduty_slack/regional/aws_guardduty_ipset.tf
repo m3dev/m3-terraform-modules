a47c53e87f958790f947d9b9733e9acc08a1802b
Seiya Yazaki <saiya.v6@gmail.com>
2019-08-22 13:53:45 +0900

    Fix IP set name to avoid update failure

--- guardduty_slack/regional/aws_guardduty_ipset.tf
+++ guardduty_slack/regional/aws_guardduty_ipset.tf
@@ -5,5 +5,14 @@ resource "aws_guardduty_ipset" "MyIPSet" {
   detector_id = "${aws_guardduty_detector.main.id}"
   format = "TXT"
   location = "${var.ipset_location}"
-  name = "terraform managed trusted IP set ${count.index}"
+  name = "terraform-${random_id.hash.hex}"
+}
+
+resource "random_id" "hash" {
+  keepers = {
+    # Generate a new id each time we switch to a new AMI id
+    ipset_location = "${var.ipset_location}"
+  }
+
+  byte_length = 8
 }
