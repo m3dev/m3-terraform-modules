075bf643322c647a7eaff7dad0b6d46e3cddd4e5
Yuichiro FUKUBAYASHI <fukubaya@gmail.com>
2020-06-03 16:41:29 +0900

    feat: add variables for aws_ecr_repository

--- ecr_repository_template/variables.tf
+++ ecr_repository_template/variables.tf
@@ -2,6 +2,12 @@ variable "repository_name" {
   description = "Name of the ECR repository"
 }
 
+variable "repository_tags" {
+  description = "Tags of the ECR repository"
+  type        = map(string)
+  default     = {}
+}
+
 variable "untagged_images_expire_days" {
   default     = 14
   description = "Automatically delete untagged images (days after push)"
