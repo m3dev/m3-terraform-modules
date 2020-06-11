91df05cd0e1bbbe77f83373e2adc40ef62567e33
ToshihiroHashimura <toshi99984@gmail.com>
2020-06-11 14:31:53 +0900

    feat: add image tag mutability setting for  aws_ecr_repository

--- ecr_repository_template/aws_ecr_repository.tf
+++ ecr_repository_template/aws_ecr_repository.tf
@@ -1,5 +1,6 @@
 resource "aws_ecr_repository" "main" {
-  name = var.repository_name
-  tags = var.repository_tags
+  name                 = var.repository_name
+  tags                 = var.repository_tags
+  image_tag_mutability = var.image_tag_mutability
 }
 
