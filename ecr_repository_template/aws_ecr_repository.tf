939389b1be9b65ff9c3d4ca08024bc583b1f7e79
Hidenobu Hayashi <hidenobu-hayashi@m3.com>
2024-05-17 11:06:54 +0900

    feat: add image scan setting for aws_ecr_repository

--- ecr_repository_template/aws_ecr_repository.tf
+++ ecr_repository_template/aws_ecr_repository.tf
@@ -2,5 +2,8 @@ resource "aws_ecr_repository" "main" {
   name                 = var.repository_name
   tags                 = var.repository_tags
   image_tag_mutability = var.image_tag_mutability
+  image_scanning_configuration {
+    scan_on_push = var.image_scan_on_push
+  }
 }
 
